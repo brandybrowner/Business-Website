c7327acbc19ea1c7f50d5012b86b520ef534a8e2
Brandy Browner <brandybwine@hotmail.com>
2015-09-13 13:36:32 -0400

    re submit

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
 		$(this).animate({opacity: 1}, 500);
 	});
 	
+
+	$(".tint").mouseenter(function() {
+		$(this).animate({opacity: 0.7}, 500);
+	});
+
+	$(".tint").mouseleave(function() {
+		$(this).animate({opacity: 1}, 500);
+	});
 });
